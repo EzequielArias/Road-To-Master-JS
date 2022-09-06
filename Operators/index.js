c88fb65f777115737b67fe7f39e7796478876e62
EzequielArias <ezequielariasdev@gmail.com>
2022-09-06 13:28:59 -0300

    Logic Operators

---
+++ Operators/index.js
@@ -0,0 +1,57 @@
+/** Tipos de operadores  Aritmeticos */
+
+let a = 5 + 5 - 10;
+
+console.log(a)
+
+//Relaciones 
+/**
+ * > Mayor que, < Menor que
+ * >= Mayor o igual, <= Menos o igual
+ * = De asignacion de valor
+ * ==   Comparacion simple
+ * ===  comparacion estricta de tipos de datos
+ * != Diferente de
+ * !==
+ */
+
+// console.log(8 > 9);
+// console.log(9 > 8);
+// console.log(8 >= 9);
+// console.log(9 >= 8);
+// console.log(7 <= 7);
+
+let trapito = "trapo"
+
+// console.log(7 == 7);
+// console.log("7" == 7);
+// console.log(trapito === "trapito")
+
+
+/*Incremento Decremento*/
+
+let i = 1;
+
+//i = i + 2
+// i += 3;
+// i/=3
+// i*=3
+// i-=3
+//operador unario
+// i++ or ++i 
+// i-- or --i
+
+// console.log(i);
+
+/* 
+Operadores Logicos
+
+ ! - NOT 
+ || - OR
+ && - AND
+*/
+
+console.log(!true);
+console.log(!false);
+console.log((9 === 9) || ("9" === 9));
+console.log((9 === 9) && ("9" === 9));
