39fa4f12540debed01fb466ac434d9bd3c18509f
EzequielArias <ezequielariasdev@gmail.com>
2022-09-09 19:24:01 -0300

    Objeto Math

---
+++ ObjectMath/index.js
@@ -0,0 +1,66 @@
+//MATH es un objeto que tiene propiedades y metodos apra constantes y funciones matematicas. No es un objeto funcion
+
+Math.E()//Constante de Euler, la base de los algoritmos naturales, aprox 2.718
+Math.LN2()//Logaritmo natural de 2, aproximadamente 0.693.
+Math.LN10()//Logaritmo natural de 10, aproximadamente 2.303
+Math.LOG2E()//logaritmo de E con base 2, aproximadamente 1.433.
+Math.PI()//Ratio de la circunferencia de un circulo respecto a su diámetro, aproximadamente 3.14159.
+Math.SQRT1_2()//Raíz cuadrada de 1/2; Equivalentemente, 1 sobre la raíz cuadrada de 2, aproximadamente 0.707.
+Math.SQRT2()//Raíz cuadrada de 2, aproximadamente 1.414.
+
+/*
+Tenga en cuenta que las funciones trigonométricas
+ (sin(), cos(), tan(), asin(), acos(), atan(), atan2())
+  devuelven ángulos en radianes.
+   Para convertir radianes a grados,
+    divida por (Math.PI / 180),
+ y multiplique por esto para convertir a la inversa.
+*/ 
+
+Math.abs()//Devuelve el valor absoluto de un número.
+Math.acos()//Devuelve el arco coseno de un número.
+Math.acosh()//Devuelve el arco coseno hiperbólico de un número.
+Math.asin()//Devuelve el arco seno de un número.
+Math.atan()//Devuelve el arco tangente de un número.
+Math.atanh()//Devuelve el arco tangente hiperbólico de un número.
+Math.atan2()//Devuelve el arco tangente del cuociente de sus argumentos.
+Math.cbrt()//Devuelve la raíz cúbica de un número.
+Math.ceil()//Devuelve el entero más pequeño mayor o igual que un número.
+Math.clz32()//Devuelve el número de ceros iniciales de un entero de 32 bits.
+Math.cos()//Devuelve el coseno de un número.
+Math.cosh()//Devuelve el coseno hiperbolico de un numero
+Math.exp()//Devuelve Ex, donde x es el argumento, y E es la constante de Euler (2.718...), la base de los logaritmos naturales.
+Math.expm1()//Devuelve ex - 1.
+Math.floor()//Devuelve el mayor entero menor que o igual a un número.
+Math.fround()//Devuelve la representacion lotante de precision simple mas cercana de un numero
+Math.hypot([x[, y[, …]]])//Devuelve la raíz cuadrada de la suma de los cuadrados de sus argumentos.
+Math.imul(x,_y)//Devuelve el resultado de una multiplicación de enteros de 32 bits.
+Math.log()//Devuelve el logaritmo natural (log, también ln) de un número.
+Math.log1p(x) (en-US)
+//Devuelve el logaritmo natural de x + 1 (loge, también ln) de un número.
+Math.log10(x)
+//Devuelve el logaritmo en base 10 de x.
+Math.log2(x)
+//Devuelve el logaritmo en base 2 de x.
+Math.max([x[, y[, …]]])
+//Devuelve el mayor de cero o más números.
+Math.min([x[, y[, …]]])
+//Devuelve el más pequeño de cero o más números.
+Math.pow(x, y)
+//Las devoluciones de base a la potencia de exponente, que es, baseexponent.
+Math.random()
+//Devuelve un número pseudo-aleatorio entre 0 y 1.
+Math.round(x)
+//Devuelve el valor de un número redondeado al número entero más cercano.
+Math.sign(x)
+//Devuelve el signo de la x, que indica si x es positivo, negativo o cero.
+Math.sin(x)
+//Devuelve el seno de un número.
+Math.sinh(x) (en-US)
+//Devuelve el seno hiperbólico de un número.
+Math.sqrt(x)
+//Devuelve la raíz cuadrada positiva de un número.
+Math.tan(x)
+//Devuelve la tangente de un número.
+Math.tanh(x)
+//Devuelve la tangente hiperbólica de un número.
