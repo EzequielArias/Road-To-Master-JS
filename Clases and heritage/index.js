408cee04a4b4d2f974a2ae44b21b19fd7a0a50d8
EzequielArias <ezequielariasdev@gmail.com>
2022-09-09 18:35:35 -0300

    clases & herencia

--- Clases and heritage/index.js
+++ Clases and heritage/index.js
@@ -21,11 +21,28 @@ class Perro extends Animal {
         //Super llama al constructor de la clase padre
         super(nombre, genero);
         this.tamanio = tamanio;
+        this.raza  = null;
     }
 
     ladrar() {
         console.log("Ladrando como un campeon");
     }
+    /*Un metodo estatico es puede ejecutar sin
+    necesidad de instanciar la clase
+    */
+   static whoAreU(){
+    console.log("I'm the choosed one");
+   }
+   //Los setterrs y getters son metodos especiales que nos
+   //permiten establecer y obtener los valores de 
+   //atributos de nuestra clase
+   get getRaza(){
+    return this.raza;
+   }
+
+   set setRaza(raza){
+    this.raza = raza;
+   }
 }
 const mimi = new Animal("Mimi","Hembra"),
       scooby = new Perro("Scooby", "Macho","Great");
@@ -35,4 +52,7 @@ const mimi = new Animal("Mimi","Hembra"),
       mimi.sonar()
 
     console.log(scooby)
-    scooby.ladrar()
+    scooby.ladrar();
+    Perro.whoAreU();
+    scooby.setRaza = "Gran danes"
+    console.log(scooby.getRaza)
