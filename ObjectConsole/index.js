3661eccb431bd671e9514b9544ed090cabd819ac
EzequielArias <ezequielariasdev@gmail.com>
2022-09-09 19:03:14 -0300

    Objeto Console

---
+++ ObjectConsole/index.js
@@ -0,0 +1,19 @@
+console.assert()//Registra un mensaje y envia una traza de error si el primer argumento es false
+console.clear()//Limpia la consola
+console.count()//Registra el numero de veces que esta linea ha sido llamada con la etiqueta dada
+console.countReset()//Reinicia el valor del contador para la etiqueta dada
+console.debug()//Regista un mensaje con nivel de debug
+console.dir()//Muestra un listado interactivo de las propiedades de un objeto Javascript especifico. Este listado permite usar triangulos de la lista desplegables para examinar el contenido de objetos hijo
+console.dirxml()//Mustra uan representacion en foram de arbol de un elemento XML/HTML si es posible o la vista del objeto Javascript si no es posible
+console.error()//Muestra un mensaje de error.Se puede utilizar sustituciones de cadenas y argumentos adicionales con este metodo
+console.group()//Cre un nuevo grupo, indentando todos los ensajes usbsecuentes en un nuevo nivel. Para retroceder un nivel, se utiliza groupEnd()
+console.groupCollapsed()//Cre un nuevo grupo, indentando todos los mensajes subsecuentes en un nuevo nivel. A diferencia de grip(), inicia con la linea de grupo colapsada, requiriendo el uso de un boton de apertura para expandir el grupo. Para retroceder un nivel, se utiliza groupEnd()
+console.info()//Muestra un mensaje de informacion en la consola. Puedes usar sustiticiones de cadenas y argumentos adicionales con este metodo
+console.log()//Para salida general de la información registrada. Puedes usar sustituciones de cadenas y argumentos adicionales con este método.
+console.profile()//Inicia el profiler incluído del navegador (por ejemplo, la Firefox performance tool). Puedes especificar un nombre opcional para el perfil.
+console.table()//Muestra los datos en forma de tabla
+console.time()//Inicia un temporizador con un nombre especificado como parametro. Hasta 10 000 temporizadores simultaneos pueden ejecutarse en una pagina determinada
+console.timeEnd()//Detiene el temporizador especificado y registra el tiempo transcurrido en milisegundos desde que fue iniciado
+console.timeLog()//Muesta el valor del temporizador especificado en la consola
+console.trace()//Muestra una traza de pila
+console.warn()//Muestra un mensaje de advertencia. Puedes usar sustituciones de cadenas y argumentos adicionales con este metodo
